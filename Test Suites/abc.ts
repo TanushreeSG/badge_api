661a1392d75cb54d0028d2ecb499a54a0b4a4de0
TanushreeSG <tanushree.sg@tibilsolutions.com>
2024-03-13 15:36:39 +0530

    added new suite

---
+++ Test Suites/abc.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>abc</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fc85c20d-b257-40eb-b94b-9bea6be71a2b</testSuiteGuid>
+</TestSuiteEntity>
